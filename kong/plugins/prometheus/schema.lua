4ceccbe8c8b9d0b5d3ab21ef978672238b5536a2
Ligreman <ligreman@users.noreply.github.com>
2021-06-14 23:21:36 +0200

    Added parameter for latency buckets configuration

--- kong/plugins/prometheus/schema.lua
+++ kong/plugins/prometheus/schema.lua
@@ -14,6 +14,13 @@ return {
         type = "record",
         fields = {
           { per_consumer = { type = "boolean", default = false }, },
+          { latency_buckets = {
+              type = "array",
+              default = { 1, 2, 5, 7, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 1000, 2000, 5000, 10000, 30000, 60000 },
+              elements = { type = "integer", between = { 1, 3600000 } },
+              len_min = 1,
+              required = true,
+          } },
         },
         custom_validator = validate_shared_dict,
     }, },
